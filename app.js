8d59020f54a5ef128fb4f36b06fd57ccec096e8e
Evan Cooper <evanncooper@gmail.com>
2017-05-03 19:28:55 -0400

    Adding event listener to hero.

---
+++ app.js
@@ -0,0 +1,10 @@
+//Get the hero!
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1')
+
+function shadow(e) {
+	console.log(e);
+}
+
+
+hero.addEventListener('mousemove', shadow);
